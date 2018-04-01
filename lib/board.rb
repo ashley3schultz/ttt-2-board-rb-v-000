fd53f987367a63dce866a9c5292f7c5a2d61ea38
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-01 21:23:49 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,7 @@
 # Define the variable board below.
-row = [" "," "," "," "," "," "," "," "," "]
+row1 = [" "," "," "]
+row2 = [" "," "," "]
+row3 = [" "," "," "]
+p1 = row1[0]
+p2 = row1[1]
+p3 = row1[3]
